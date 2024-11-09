ef3abbd1307a685cd2b06b10f3b475bfa4344bb5
Sam Pullman <sampullman@gmail.com>
2024-11-09 20:16:35 +0800

    fix: ethers exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,23 @@
 export * from './util'
 
 export { useChain } from './use-chain'
+
+import {
+  hexlify,
+  isHexString,
+  isAddress,
+  formatEther,
+  formatUnits,
+  parseEther,
+  parseUnits,
+} from 'ethers'
+
+export {
+  hexlify,
+  isHexString,
+  isAddress,
+  formatEther,
+  formatUnits,
+  parseEther,
+  parseUnits,
+}
--- lib/providers/index.ts
+++ lib/providers/index.ts
@@ -2,23 +2,3 @@ export * from './i-provider'
 export * from './provider-config'
 export * from './use-metamask'
 export * from './use-walletconnect'
-
-import {
-  hexlify,
-  isHexString,
-  isAddress,
-  formatEther,
-  formatUnits,
-  parseEther,
-  parseUnits,
-} from 'ethers'
-
-export const ethers = {
-  hexlify,
-  isHexString,
-  isAddress,
-  formatEther,
-  formatUnits,
-  parseEther,
-  parseUnits,
-}
