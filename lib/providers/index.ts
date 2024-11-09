b5a4bea8c06de63d5ba4c6eef21d68b84255905a
Sam Pullman <sampullman@gmail.com>
2024-11-09 18:26:48 +0800

    feat: re-export a few ethers utils

--- lib/providers/index.ts
+++ lib/providers/index.ts
@@ -2,3 +2,23 @@ export * from './i-provider'
 export * from './provider-config'
 export * from './use-metamask'
 export * from './use-walletconnect'
+
+import {
+  hexlify,
+  isHexString,
+  isAddress,
+  formatEther,
+  formatUnits,
+  parseEther,
+  parseUnits,
+} from 'ethers'
+
+export const ethers = {
+  hexlify,
+  isHexString,
+  isAddress,
+  formatEther,
+  formatUnits,
+  parseEther,
+  parseUnits,
+}
